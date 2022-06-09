30ca9152b8abba6064bf404a3b9ae6cde865ed62
headsink <headsink@gmail.com>
2022-06-09 21:22:09 +0800

    setting update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	title: `headsink's portal`,
+	description: `Just something simple for now`,
+	url: `https://localhost:3000`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
 };
